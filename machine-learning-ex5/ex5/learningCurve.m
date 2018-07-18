122390d61a2d856966b1c1856698fe6fecde2995
unknown <hinanmu@gmail.com>
2018-07-18 15:46:28 +0800

    update ex5 learningCurve

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -53,8 +53,12 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
-
-
+for i = 1:m
+  theta = trainLinearReg(X(1:i, :), y(1:i), lambda);
+  error_train(i) = 1 / (2 * i) * sum((X(1:i, :) * theta - y(1:i)).^2);
+  error_val(i) = 1 / (2 * size(Xval, 1)) * sum((Xval * theta - yval).^2);
+  
+endfor
 
 
 
